bff2cb1df0943431141a6ea8dc146818e0a81085
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2020-02-20 14:20:24 -0700

    My Scripts

---
+++ Scripts/DivingPlayerState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivingPlayerState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        Debug.Log("Entered diving");
+        player.mCurrentState = this;
+        Rigidbody rbPlayer = player.GetComponent<Rigidbody>();
+        rbPlayer.transform.localScale *= 0.5f;
+    }
+
+    public void Execute(Player player)
+    {
+        if (!Input.GetKey(KeyCode.S))
+        {
+            // transition to standing
+            StandingPlayerState standingState = new StandingPlayerState();
+            Rigidbody rbPlayer = player.GetComponent<Rigidbody>();
+            rbPlayer.transform.localScale *= 2.0f;
+            standingState.Enter(player);
+        }
+    }
+}
---
+++ Scripts/JumpingPlayerState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlayerState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        Debug.Log("Entered jumping");
+        player.mCurrentState = this;
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        rb.AddForce(0, 400 * Time.deltaTime, 0, ForceMode.VelocityChange);
+    }
+
+    public void Execute(Player player)
+    {
+        if(Physics.Raycast(player.transform.position, Vector3.down, 0.55f) && !Input.GetKey(KeyCode.S))
+        {
+            StandingPlayerState standingState = new StandingPlayerState();
+            standingState.Enter(player);
+        } else if(Input.GetKeyDown(KeyCode.S))
+        {
+            // transition to diving
+            DivingPlayerState divingState = new DivingPlayerState();
+            divingState.Enter(player);
+        }
+    }
+}
